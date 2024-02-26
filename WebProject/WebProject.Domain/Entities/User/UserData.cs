596c0fd33d81e027b691877d6de6df30263d3956
Stoian-Chiril <chiril.stoian@isa.utm.md>
2024-02-26 17:20:05 +0200

    Added Projects etc.
    Added Domain, Helper, BusinessLogic. some folders in them, item UserData

---
+++ WebProject/WebProject.Domain/Entities/User/UserData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebProject.Domain.Entities.User
+{
+    internal class UserData
+    {
+        public int Credential { get; set; }
+        public string Password { get; set; }
+        public string LoginIp { get; set; }
+        public DateTime LoginDataTime { get; set; }
+    }
+}
---
+++ WebProject/WebProject/models/UserData.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace WebProject.Models
+{
+    public class UserData
+    {
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
