5ee14372292175200210a5a1aab8a506af3038e2
Alexandr Pekel <119462299+RaphaelsHub@users.noreply.github.com>
2024-03-12 19:21:12 +0200

    Ended Rigistration Page
    Here I connected Model RegistrationData with SignUp View

--- WebProject/WebProject/Controllers/SignUpController.cs
+++ WebProject/WebProject/Controllers/SignUpController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using WebProject.Models;
+using WebProject.DateAccesClasses;
 
 namespace WebProject.Controllers
 {
@@ -13,5 +16,28 @@ namespace WebProject.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Registration(RegistrationData registrationData)
+        {
+            // Обработка введенных данных
+
+            bool userExists = CheckIfExistUser.Check(registrationData);
+
+            if (userExists)
+            {
+                ViewBag.ErrorMessage1 = "User with this email already exists.";
+                return View();
+            }
+            else
+            {
+                //запись в базу
+                HomeController.IsAuthorized = true;
+
+                // После успешной регистрации перенаправляем пользователя на главную страницу
+                return RedirectToAction("Index", "Home");
+            }
+        }
     }
+
 }
---
+++ WebProject/WebProject/DateAccesClasses/CheckIfExistUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using WebProject.Models;
+
+namespace WebProject.DateAccesClasses
+{
+
+public class CheckIfExistUser
+{
+    static public bool Check(RegistrationData data)
+    {
+        // Проверяем, что данные не пустые
+        if (string.IsNullOrEmpty(data.Email) || 
+            string.IsNullOrEmpty(data.Name) || 
+            string.IsNullOrEmpty(data.Phone) || 
+            string.IsNullOrEmpty(data.Password))
+        {
+            return true; // Если хотя бы одно поле пусто, то считаем, что пользователь существует
+        }
+        
+        return false; // Если все поля заполнены, то считаем, что пользователь не существует
+    }
+}
+
+}
