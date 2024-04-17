7dc501266e91a8e2c57910e9249668fd7d4a1a12
ZEN5072 <keygiver502@gmail.com>
2024-04-17 16:04:56 +0300

    Create ProductResponse
    Create ProductResponse for ProductAPI

---
+++ WebProject/WebProject.BusinessLogic/Core/Levels/ProductResponse/ProductResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebProject.ModelAccessLayer.Model;
+
+namespace WebProject.BusinessLogic.Core.Levels.ProductResponse
+{
+    public class ProductResponse
+    {
+        public Product Data { get; set; }
+        public bool IsExist { get; set; }
+        public string ResponseMessage { get; set; }
+    }
+}
