399ea763f89655923b611780945e4ab98d3fc0aa
ZEN5072 <keygiver502@gmail.com>
2024-04-17 16:01:23 +0300

    Create StandartResponse PageInfo
    Create StandartResponse and PageInfo for productAPI

---
+++ WebProject/WebProject.BusinessLogic/Core/Levels/ProductResponse/PageInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebProject.BusinessLogic.Core.Levels.ProductResponse
+{
+    public class PageInfo
+    {
+        public uint ProductsPerPage { get; set; }
+        public uint PageIndex { get; set; }
+    }
+}
