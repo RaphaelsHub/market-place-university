e3756059c7405ce5cd6e3be54e1188e2fdb7a86c
Stoian-Chiril <chiril.stoian@isa.utm.md>
2024-04-29 16:44:08 +0300

    Add response

---
+++ WebProject/WebProject.BusinessLogic/Core/Levels/GeneralResponse/ResponseUserStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebProject.BusinessLogic.Core.Levels.GeneralResponse
+{
+    public class ResponseUserStatus
+    {
+        public bool Status { get; set; }
+        public string ResponseMessage { get; set; }
+        public bool IsAdmin { get; set; }
+        public int ID { get; set; }
+    }
+}
