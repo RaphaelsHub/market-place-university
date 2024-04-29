8879ef0b0e40db53f70bc487feab3b86ae24fe1a
Stoian-Chiril <chiril.stoian@isa.utm.md>
2024-04-29 20:37:23 +0300

    Create OrderEF and CartItemEF

---
+++ WebProject/WebProject.Domain/Entities/DBModels/CartItemEF.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebProject.Domain.Entities.User;
+
+namespace WebProject.Domain.Entities.DBModels
+{
+    public class CartItemEF
+    {
+        //[Key]
+        public int CartItemId { get; set; }
+        public int UserId { get; set; } // Внешний ключ для связи с таблицей пользователей
+        public int ProductId { get; set; } // Внешний ключ для связи с таблицей товаров
+
+
+        //[ForeignKey("ProductId")]
+        //public virtual Product Product { get; set; } // Навигационное свойство для доступа к связанному товару
+        public int Quantity { get; set; } // Количество конкретного товара
+
+        //[ForeignKey("UserId")]
+        public virtual UserEF User { get; set; } // Навигационное свойство для доступа к связанному пользователю
+
+    }
+}
---
+++ WebProject/WebProject.Domain/Entities/DBModels/OrderEF.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebProject.Domain.Entities.User;
+
+namespace WebProject.Domain.Entities.DBModels
+{
+    public class OrderEF
+    {
+        //[Key]
+        public int Order_Id { get; set; }
+        public int UserId { get; set; } // Внешний ключ для связи с таблицей Users
+        public int AdminId { get; set; } // Внешний ключ для связи с таблицей Users
+        public DateTime OrderDate { get; set; }
+        public ICollection<CartItemEF> CartItems { get; set; }
+
+        //[ForeignKey("UserId")]
+        public virtual UserEF User { get; set; } // Навигационное свойство для доступа к связанному пользователю
+
+        //[ForeignKey("AdminId")]
+        public virtual AdminEF Admin { get; set; } // Навигационное свойство для доступа к связанному владельцу
+    }
+}
