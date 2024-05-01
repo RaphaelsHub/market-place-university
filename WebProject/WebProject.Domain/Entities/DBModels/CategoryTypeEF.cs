7bbb760dc278bc16f88bdae8a4af607fbafbedd9
ZEN5072 <keygiver502@gmail.com>
2024-05-01 03:37:07 +0300

    Create CategoryTypeEF.cs

---
+++ WebProject/WebProject.Domain/Entities/DBModels/CategoryTypeEF.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebProject.Domain.Entities.DBModels
+{
+    //мега крутая рекурсивная архитектура
+    //дэ факто архитектура дерева
+    public class CategoryTypeEF
+    {
+        //[Key]
+        public int CategoryTypeId { get; set; }
+        public string CategoryName { get; set; }
+
+        // Коллекция фильтров для катеогрии товара
+        // public virtual ICollection<Filter> Filters { get; set; }
+
+
+        // Навигационное свойство на родительскую категорию
+        public int? ParentCategoryId { get; set; }
+
+        //[ForeignKey("ParentCategoryId")]
+        public virtual CategoryTypeEF ParentCategory { get; set; }
+
+        // Коллекция дочерних категорий
+        public virtual ICollection<CategoryTypeEF> ChildCategories { get; set; }
+    }
+}
