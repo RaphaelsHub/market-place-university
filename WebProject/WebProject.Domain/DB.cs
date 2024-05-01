61fe2f4da8bd681c683193455076f5fb26dd3ecf
ZEN5072 <keygiver502@gmail.com>
2024-05-01 05:08:18 +0300

    Create base for DB
    also make local Constants to connect to MSSQL server

---
+++ WebProject/WebProject.Domain/DB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebProject.Domain.Entities.DBModels;
+using WebProject.Domain.Entities.User;
+
+namespace WebProject.Domain
+{
+    internal class DB
+    {
+        //public class Context : DbContext
+        //{
+        //    public DbSet<ProductDataEF> Products { get; set; }
+        //    public DbSet<CategoryTypeEF> CategoryTypes { get; set; }
+        //    public Context() : base(Constants.SqlConnectionSQLServer) //подключение к mdl3.bayracraft.co.in
+        //    {
+
+        //    }
+        //}
+    }
+}
