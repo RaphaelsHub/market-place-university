d3e04fef71d3e1f7a4deac4676e9720b295e37ff
ZEN5072 <keygiver502@gmail.com>
2024-05-03 12:18:41 +0300

    Update AdminBL
    Create_Convertashion metods
    ======================
    Create ConvertProductDataEF
    Create ConvertProduct
    Create ConvertAllProducts
    
    =========================
    Copy Chiril Conv Metod from GuestBL

--- WebProject/WebProject.BusinessLogic/MainBL/AdminBL.cs
+++ WebProject/WebProject.BusinessLogic/MainBL/AdminBL.cs
@@ -8,6 +8,10 @@ using WebProject.BusinessLogic.Core.Levels;
 using WebProject.BusinessLogic.Interfaces;
 using WebProject.ModelAccessLayer.Model;
 
+using WebProject.Domain.Entities.DBModels;
+using WebProject.Domain.Entities.User;
+using WebProject.Domain.Enum;
+
 namespace WebProject.BusinessLogic.MainBL
 {
     public class AdminBL : IAdmin, IUserBase
@@ -72,6 +76,154 @@ namespace WebProject.BusinessLogic.MainBL
         public AllDeliveries ViewOrders(int indexUser)
         {
             return null;
+        }
+    
+        //Convert
+        static private ProductDataEF ConvertProductDataEF(Product product)
+        {
+            var productEF = new ProductDataEF
+            {
+                Name = product.Name,
+                Details = product.Details,
+                ShortDescription = product.ShortDescription,
+                FullDescription = product.FullDescription,
+                Price = product.Price,
+                Amount = product.Amount,
+            };
+            productEF.SetPhotos(product.PhotoUrl);
+            return productEF;
+        }
+
+        static private Product ConvertProduct(ProductDataEF data)
+        {
+            return new Product
+            {
+                Id = data.Id,
+                Name = data.Name,
+                Details = data.Details,
+                filter = null, // По вопросам к Саше или Ивану
+                ShortDescription = data.ShortDescription,
+                FullDescription = data.FullDescription,
+                PhotoUrl = data.GetPhotos(),
+                Price = data.Price,
+                Amount = data.Amount
+            };
+        }
+
+        static private AllProducts ConvertAllProducts(List<ProductDataEF> listData)
+        {
+            var allProductList = new List<Product>();
+            foreach (ProductDataEF productData in listData)
+            {
+                allProductList.Add(ConvertProduct(productData));
+            }
+
+            return new AllProducts { Products = allProductList };
+        }
+
+
+        //Chirill metods
+        static private Dictionary<string, StatusDelivery> StatusDeliveryDictionary = new Dictionary<string, StatusDelivery>
+        {
+            {"Pending", StatusDelivery.Pending},                // Ожидание
+            {"Processing" , StatusDelivery.Processing},         // Обработка
+            {"Shipped" , StatusDelivery.Shipped},               // Отправлено
+            {"OutForDelivery" , StatusDelivery.OutForDelivery },// В пути
+            {"Delivered" , StatusDelivery.Delivered },          // Доставлено
+            {"Returned" , StatusDelivery.Returned },            // Возвращено
+            {"Canceled" , StatusDelivery.Canceled }             // Отменено
+        };
+        static private Product GenerateProduct(ProductDataEF data)
+        {
+            return new Product
+            {
+                Id = data.Id,
+                Name = data.Name,
+                Details = data.Details,
+                FullDescription = data.FullDescription,
+                ShortDescription = data.ShortDescription,
+                Amount = data.Amount,
+                Price = data.Price,
+                filter = null,
+                PhotoUrl = null // написать ване
+            };
+        }
+        static private CartData GenerateCartData(ICollection<CartItemEF> userCart)
+        {
+            List<Tuple<Product, int>> productList = new List<Tuple<Product, int>>();
+            decimal sumPrice = 0;
+            decimal deliveryPrice = 0;
+            decimal finalPrice = 0;
+
+            foreach (CartItemEF cartItem in userCart)
+            {
+                var par = new Tuple<Product, int>(
+                    GenerateProduct(cartItem.Product),
+                    cartItem.Quantity);
+                sumPrice += cartItem.Product.Price * cartItem.Quantity;
+                productList.Add(par);
+            }
+            finalPrice = sumPrice + deliveryPrice;
+            return new CartData
+            {
+                SumPrice = sumPrice,
+                DeliveryPrice = deliveryPrice,
+                FinalPrice = finalPrice,
+                productList = productList
+            };
+        }
+        static private StatusDelivery GenerateStatusDelivery(string RawOrderStatusEF)
+        {
+            if (AdminBL.StatusDeliveryDictionary.TryGetValue(RawOrderStatusEF, out StatusDelivery result))
+            {
+                return result;
+            }
+            else
+            {
+                return 0; //нужно придумать возврат некоректных данных
+            }
+        }
+        static private OrderModel GenerateOrderModel(OrderEF data)
+        {
+            var orderInfo = new OrderInfo
+            {
+                UserId = data.UserId,
+
+                Name = data.Name,
+                Email = data.Email,
+                Phone = data.Phone,
+                Country = data.Country,
+                City = data.City,
+                Address = data.Address,
+                Comment = data.Comment,
+            };
+            var cardCred = new CardCreditionals();
+            var cartData = GenerateCartData(data.CartItems);
+
+            var StatusDelivery = GenerateStatusDelivery(data.StatusDelivery);
+
+            return new OrderModel
+            {
+                Id = data.Order_Id,
+                OrderInfo = orderInfo,
+                CardCreditinals = cardCred,
+                CartData = cartData,
+                StatusDelivery = StatusDelivery
+            };
+
+        }
+        static private AllDeliveries GenerateDeliveries(ICollection<OrderEF> orders)
+        {
+            var deliverisList = new List<OrderModel>();
+
+            foreach (var order in orders)
+            {
+                deliverisList.Add(GenerateOrderModel(order));
+            }
+            return new AllDeliveries
+            {
+                AllOrders = deliverisList
+            };
         }
     }
 }
