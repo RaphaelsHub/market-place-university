f53fe5a079b46df3cd7c0b95cc75e5eba1eaab4d
Stoian-Chiril <chiril.stoian@isa.utm.md>
2024-05-06 02:36:55 +0300

    Done UserBL

--- WebProject/WebProject.BusinessLogic/MainBL/UserBL.cs
+++ WebProject/WebProject.BusinessLogic/MainBL/UserBL.cs
@@ -6,22 +6,24 @@ using System.Text;
 using System.Threading.Tasks;
 using WebProject.BusinessLogic.Core;
 using WebProject.BusinessLogic.Core.Levels;
+using WebProject.BusinessLogic.Core.Levels.GeneralResponse;
 using WebProject.BusinessLogic.Interfaces;
 using WebProject.Domain.Entities.DBModels;
+using WebProject.Domain.Enum;
 using WebProject.ModelAccessLayer.Model;
 
 namespace WebProject.BusinessLogic.MainBL
 {
     public class UserBL : UserAPI, IUserBase
     {
-        private ProductAPI productAPI = new ProductAPI();
         public bool AddToCart(CartItem cartItem)
         {
             var responseUser = FindUserEF(cartItem.Id_User);
             if (responseUser.IsExist == false)
                 return false;
 
-            var responseProduct = productAPI.GetSingleProductData(cartItem.Id);
+            var prodAPI = new ProductAPI();
+            var responseProduct = prodAPI.GetSingleProductData(cartItem.Id);
             if (responseProduct.IsExist == false)
                 return false;
 
@@ -41,6 +43,7 @@ namespace WebProject.BusinessLogic.MainBL
 
             return true;
         }
+        
         public bool DeleteFromCart(CartItem cartItem)
         {
             var deletedCartItemEFResponse = FindCartItemEF(cartItem.Id, cartItem.Id_User);
@@ -56,17 +59,164 @@ namespace WebProject.BusinessLogic.MainBL
 
         public bool ProcessOrder(OrderModel orderModel)
         {
+            var orderInfoReqest = GenerateEFOrderInfo(orderModel);
+            var response = ProcessUserOrder(orderModel.OrderInfo.UserId, orderInfoReqest);
+            if (response.IsExist == false) 
+                return false;
+
             return true;
         }
 
         public CartData ViewCart(int indexUser)
         {
-            return null;
+            var response = ViewUserCart(indexUser);
+            if(response.IsExist == false) 
+                return null;
+
+            return GenerateCartData(response.Data);
         }
 
         public AllDeliveries ViewOrders(int indexUser)
         {
-            return null;
+            var response = ViewUserOrders(indexUser);
+            if(response.IsExist == false) 
+                return null;
+
+            return GenerateDeliveries(response.Data);
+        }
+
+
+        //преобразования EntityFramework моделей в ASP.Net модели и наоборот
+        static private Dictionary<string, StatusDelivery> StatusDeliveryDictionary = new Dictionary<string, StatusDelivery>
+        {
+            {"Pending", StatusDelivery.Pending},                // Ожидание
+            {"Processing" , StatusDelivery.Processing},         // Обработка
+            {"Shipped" , StatusDelivery.Shipped},               // Отправлено
+            {"OutForDelivery" , StatusDelivery.OutForDelivery },// В пути
+            {"Delivered" , StatusDelivery.Delivered },          // Доставлено
+            {"Returned" , StatusDelivery.Returned },            // Возвращено
+            {"Canceled" , StatusDelivery.Canceled }             // Отменено
+        };
+        static private StatusDelivery GenerateStatusDelivery(string RawOrderStatusEF)
+        {
+            if (StatusDeliveryDictionary.TryGetValue(RawOrderStatusEF, out StatusDelivery result))
+            {
+                return result;
+            }
+            else
+            {
+                return 0; //нужно придумать возврат некоректных данных
+            }
+        }
+        static private Dictionary<StatusDelivery, string> ReverseStatusDeliveryDictionary = new Dictionary<StatusDelivery, string>
+        {
+            {StatusDelivery.Pending,    "Pending"},             // Ожидание
+            {StatusDelivery.Processing, "Processing"},         // Обработка
+            {StatusDelivery.Shipped,    "Shipped"},           // Отправлено
+            {StatusDelivery.OutForDelivery, "OutForDelivery"},// В пути
+            {StatusDelivery.Delivered , "Delivered"},        // Доставлено
+            {StatusDelivery.Returned,   "Returned"},        // Возвращено
+            {StatusDelivery.Canceled ,  "Canceled"}        // Отменено
+        };
+        private static OrderInfoReqest GenerateEFOrderInfo(OrderModel order)
+        {
+            var orderInfo = order.OrderInfo;
+            string stringStatusDelivery = "Canceled";
+            if (ReverseStatusDeliveryDictionary.ContainsKey(order.StatusDelivery))
+                stringStatusDelivery = ReverseStatusDeliveryDictionary[order.StatusDelivery];
+            return new OrderInfoReqest
+            {
+                OrderId = order.Id,
+                Name = orderInfo.Name,
+                Email = orderInfo.Email,
+                Phone = orderInfo.Phone,
+                Country = orderInfo.Country,
+                City = orderInfo.City,
+                Address = orderInfo.Address,
+                Comment = orderInfo.Comment,
+                StatusDelivery = stringStatusDelivery
+            };
+        }
+        static private Product GenerateProduct(ProductDataEF data)
+        {
+            return new Product
+            {
+                Id = data.Id,
+                Name = data.Name,
+                Details = data.Details,
+                FullDescription = data.FullDescription,
+                ShortDescription = data.ShortDescription,
+                Amount = data.Amount,
+                Price = data.Price,
+                filter = null,
+                PhotoUrl = null // написать ване
+            };
+        }
+        static private CartData GenerateCartData(ICollection<CartItemEF> userCart)
+        {
+            List<Tuple<Product, int>> productList = new List<Tuple<Product, int>>();
+            decimal sumPrice = 0;
+            decimal deliveryPrice = 0;
+            decimal finalPrice = 0;
+
+            foreach (CartItemEF cartItem in userCart)
+            {
+                var par = new Tuple<Product, int>(
+                    GenerateProduct(cartItem.Product),
+                    cartItem.Quantity);
+                sumPrice += cartItem.Product.Price * cartItem.Quantity;
+                productList.Add(par);
+            }
+            finalPrice = sumPrice + deliveryPrice;
+            return new CartData
+            {
+                SumPrice = sumPrice,
+                DeliveryPrice = deliveryPrice,
+                FinalPrice = finalPrice,
+                productList = productList
+            };
+        }
+        static private OrderModel GenerateOrderModel(OrderEF data)
+        {
+            var orderInfo = new OrderInfo
+            {
+                UserId = data.UserId,
+
+                Name = data.Name,
+                Email = data.Email,
+                Phone = data.Phone,
+                Country = data.Country,
+                City = data.City,
+                Address = data.Address,
+                Comment = data.Comment,
+            };
+            var cardCred = new CardCreditionals();
+            var cartData = GenerateCartData(data.CartItems);
+
+            var StatusDelivery = GenerateStatusDelivery(data.StatusDelivery);
+
+            return new OrderModel
+            {
+                Id = data.Order_Id,
+                OrderInfo = orderInfo,
+                CardCreditinals = cardCred,
+                CartData = cartData,
+                StatusDelivery = StatusDelivery
+            };
+
+        }
+        static private AllDeliveries GenerateDeliveries(List<OrderEF> orders)
+        {
+            var deliverisList = new List<OrderModel>();
+
+            foreach (var order in orders)
+            {
+                deliverisList.Add(GenerateOrderModel(order));
+            }
+            return new AllDeliveries
+            {
+                AllOrders = deliverisList
+            };
         }
     }
 }
