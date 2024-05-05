891ebf9196d0366ac84951c37e976d4161054e97
Stoian-Chiril <chiril.stoian@isa.utm.md>
2024-05-06 01:45:16 +0300

    Update UserBL

--- WebProject/WebProject.BusinessLogic/MainBL/UserBL.cs
+++ WebProject/WebProject.BusinessLogic/MainBL/UserBL.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WebProject.BusinessLogic.Core;
+using WebProject.BusinessLogic.Core.Levels;
 using WebProject.BusinessLogic.Interfaces;
 using WebProject.Domain.Entities.DBModels;
 using WebProject.ModelAccessLayer.Model;
@@ -12,8 +13,19 @@ namespace WebProject.BusinessLogic.MainBL
 {
     public class UserBL : UserAPI, IUserBase
     {
+        private ProductAPI productAPI = new ProductAPI();
         public bool AddToCart(CartItem cartItem)
         {
+            var responseProduct = productAPI.GetSingleProductData(cartItem.Id);
+            if (responseProduct.IsExist == false)
+                return false;
+
+            //var responseUser = 
+
+            //CartItemEF newCartItemEF =  new CartItemEF { 
+            //    UserId = cartItem.Id_User,
+            //    Quantity = cartItem.Quantity };
+
 
             return true;
         }
