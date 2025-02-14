71af8c9445a10ab5259c914b45d5a622ee552dd0
Alexandr Pekel <spekel2003@gmail.com>
2025-02-14 23:14:25 +0200

    AuthenticationService.cs implemented login, register, logOut.

--- ECommerce/ECommerce.App/Services/Auth/AuthenticationService.cs
+++ ECommerce/ECommerce.App/Services/Auth/AuthenticationService.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Threading.Tasks;
+using System.Web;
+using ECommerce.App.Infrastructure.Abstractions;
 using ECommerce.App.Interfaces.Auth;
+using ECommerce.App.Interfaces.JWT;
 using ECommerce.Core.DataTransferObjects.AuthDto;
 using ECommerce.Core.DataTransferObjects.Responses;
 using ECommerce.Core.Entities.User;
@@ -11,31 +14,69 @@ namespace ECommerce.App.Services.Auth
 {
     public class AuthenticationService : IAuthenticationService
     {
-        private readonly UsersRepository _usersRepository;
+        private readonly IUsersRepository _usersRepository;
+        private readonly IJwtService _jwtService;
+        private readonly ICookiesService _cookiesService;
         
-        public AuthenticationService(UsersRepository usersRepository)
+        public AuthenticationService(IUsersRepository usersRepository, ICookiesService cookiesService, IJwtService jwtService)
         {
             _usersRepository = usersRepository;
-        }
-        
-        public Task<ResponseType1<bool>> SignUp(SignUpDto registrationDto)
-        {
-            throw new System.NotImplementedException();
+            _cookiesService = cookiesService;
+            _jwtService = jwtService;
         }
 
-        public Task<ResponseType1<bool>> SignIn(SignInDto loginDto)
+        public async Task<ResponseType1<bool>> SignUp(SignUpDto registrationDto)
         {
-            throw new Exception("Not implemented");
+            var user = await _usersRepository.GetByEmailAsync(registrationDto.Email);
+            
+            if (user != null)
+                return new ResponseType1<bool>(false, "User with this email already exists");
+            
+            var newUser = new UserEf
+            {
+                Name = registrationDto.Name,
+                Email = registrationDto.Email,
+                PasswordHash = HashingUtility.HashPasswordSha256(registrationDto.Password),
+                IsSignUpForNewsletter = registrationDto.SignUpForNewsletter,
+                IpAddress = HttpContext.Current.Request.UserHostAddress
+            };
+
+            await _usersRepository.CreateAsync(newUser);
+
+            var user2 = await _usersRepository.GetByEmailAsync(registrationDto.Email);
+            
+            var token = _jwtService.GenerateToken(user2);
+            
+            await _cookiesService.SetCookie("jwt", token, DateTime.Now.AddHours(1));
+            
+            return new ResponseType1<bool>(true, "User created successfully");
         }
 
-        public Task<ResponseType1<bool>> Logout()
+        public async Task<ResponseType1<bool>> SignIn(SignInDto loginDto)
         {
-            throw new System.NotImplementedException();
+            var user = await _usersRepository.GetByEmailAsync(loginDto.Email);
+            
+            if (user == null || !HashingUtility.VerifyPasswordSha256(loginDto.Password, user.PasswordHash))
+                return new ResponseType1<bool>(false, "Email or password is incorrect.");
+            
+
+            
+            var token = _jwtService.GenerateToken(user);
+            
+            await _cookiesService.SetCookie("jwt", token, DateTime.Now.AddHours(1));
+
+            return new ResponseType1<bool>(true, "User signed in successfully");
+        }
+
+        public async Task<ResponseType1<bool>> Logout()
+        {
+            await _cookiesService.DeleteCookie("jwt");
+            return new ResponseType1<bool>(true, "User signed out successfully");
         }
 
         public Task<ResponseType1<bool>> SendEmailConfirmation(string email)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
     }
 }
