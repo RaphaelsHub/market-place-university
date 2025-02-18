77e4085c3d69dacb19fdc9be3bccd3c5af9f5794
Alex <119462299+RaphaelsHub@users.noreply.github.com>
2025-02-17 22:45:17 -0800

    Created ManagementBlogsController.cs and views.

---
+++ ECommerce/ECommerce.Api/Controllers/ManagementBlogsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using ECommerce.Core.Models.DTOs.Blog;
+
+namespace ECommerce.Controllers
+{
+    public class ManagementBlogsController : BaseController
+    {
+        [HttpGet] public ActionResult Index(string searchByName="", int page = 1, int usersPerPage= 100) => View();
+        [HttpGet] public ActionResult ViewBlog(int id) => View();
+        
+        [HttpPost] public ActionResult AddBlog(BlogDto blogDto) => RedirectToAction("Index", "ManagementBlogs");
+        
+        [HttpPost] public ActionResult UpdateBlog(BlogDto blogDto) => RedirectToAction("Index", "ManagementBlogs");
+        
+        [HttpPost] public ActionResult DeleteBlog(int idBlog) => RedirectToAction("Index", "ManagementBlogs");
+    }
+}
