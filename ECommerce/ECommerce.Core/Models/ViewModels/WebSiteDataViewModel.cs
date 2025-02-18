a0ee7adeb98c219190c724acbe7665368127065c
Alex <119462299+RaphaelsHub@users.noreply.github.com>
2025-02-18 08:56:29 -0800

    WebSiteDataViewModel.cs added.

---
+++ ECommerce/ECommerce.Core/Models/ViewModels/WebSiteDataViewModel.cs
@@ -0,0 +1,20 @@
+namespace ECommerce.Core.Models.ViewModels
+{
+    public class WebSiteDataViewModel
+    {
+        public string ShopLogo { get; set; }
+        public string ShopPhoto { get; set; }
+        public string WorkingAddressShop { get; set; }
+        public string WorkingNameShop { get; set; }
+        public string ShopDescription { get; set; }
+        public string InfoEmail { get; set; }
+        public string SupportEmail { get; set; }
+        public string WorkingEmail { get; set; }
+        public uint InfoNumber { get; set; }
+        public uint SupportNumber { get; set; }
+        public uint WorkingNumber { get; set; }
+        public string InstagramLink { get; set; }
+        public string FacebookLink { get; set; }
+        public string XLink { get; set; }
+    }
+}
