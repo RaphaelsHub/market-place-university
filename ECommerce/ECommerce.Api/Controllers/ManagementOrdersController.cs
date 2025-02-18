2c3a0acd83f861af101b6ab690924c016e61cdeb
Alex <119462299+RaphaelsHub@users.noreply.github.com>
2025-02-17 22:41:04 -0800

    Created ManagementOrdersController.cs and related views.

---
+++ ECommerce/ECommerce.Api/Controllers/ManagementOrdersController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using ECommerce.Core.Models.DTOs.User;
+
+namespace ECommerce.Controllers
+{
+    public class ManagementOrdersController : Controller
+    {
+        [HttpGet] public ActionResult Index(string searchByOrderName="", string time ="", string sort="", int page = 1, int usersPerPage= 100) => View();
+        
+        [HttpGet] public ActionResult ViewOrder(int id) => View();
+        
+        [HttpPost] public ActionResult AddOrder(UserDto userDto) => RedirectToAction("Index", "ManagementOrders");
+        
+        [HttpPost] public ActionResult UpdateOrder(UserDto userDto) => RedirectToAction("Index", "ManagementOrders");
+        
+        [HttpPost] public ActionResult DeleteOrder(int idUser) => RedirectToAction("Index", "ManagementOrders");
+    }
+}
