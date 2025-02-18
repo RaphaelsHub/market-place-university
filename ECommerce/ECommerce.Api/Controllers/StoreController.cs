a69fe31e4ad8b6c26a65c0def306a18454e3d455
Alex <119462299+RaphaelsHub@users.noreply.github.com>
2025-02-18 08:57:14 -0800

    StoreController.cs, views.

--- ECommerce/ECommerce.Api/Controllers/StoreController.cs
+++ ECommerce/ECommerce.Api/Controllers/StoreController.cs
@@ -1,66 +1,10 @@
-﻿using System.Web.Mvc;
-using ECommerce.App.Interfaces.Product;
-using ECommerce.Core.DataTransferObjects;
+using System.Web.Mvc;
 
 namespace ECommerce.Controllers
 {
-    public class StoreController : Controller
+    public class StoreController : BaseController
     {
-        private readonly IProductService _storeService;
-
-        public StoreController(IProductService storeService)
-        {
-            _storeService = storeService;
-        }
-
-
-        [HttpGet] public ActionResult Product(int? id)
-        {
-            if (id == null)
-                return RedirectToAction("Products");
-            
-            var product = _storeService.GetProductAsync(id.Value);
-            
-            return View(product);
-        }
-
-        [HttpGet] public ActionResult Products(int page = 1) => View(_storeService.GetProductsAsync(1, page).Result.Data);
-        
-        [HttpPost] public ActionResult AddProduct(ProductDto productDto)
-        {
-            if (!ModelState.IsValid)
-                return View(productDto);
-
-            var response = _storeService.CreateProductAsync(productDto);
-            
-
-            
-            return RedirectToAction("Products",response.Result.Data);
-        }
-        
-        [HttpPatch]
-        public ActionResult UpdateProduct() => RedirectToAction("Products");
-        
-        [HttpDelete]
-        public ActionResult DeleteProduct() => RedirectToAction("Products");
-        
-        
-        // Find Product by name and Id
-        [HttpGet]
-        public ActionResult FindProductById(int id) => RedirectToAction("Product", new object());
-        
-        [HttpGet]
-        public ActionResult FindProductByName(string name) => RedirectToAction("Product", new object());
-        
-        
-        // Sort Products by Category and typeSearch
-        [HttpGet]
-        public ActionResult SortByCategory(string category) => RedirectToAction("Products", category);
-        
-        [HttpGet]
-        public ActionResult SortBy(string sort)
-        {
-            return RedirectToAction("Products", "Store", sort);
-        }
+        [HttpGet] public ActionResult Index(string name = "", string category = "", string sort = "", int? page = 1, int? itemsPerPage = 10 ) => View();
+        [HttpGet] public ActionResult Product(int? idProduct) => View();
     }
 }
