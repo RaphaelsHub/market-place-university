07187a9e2e7e1bf50e4aef477ef425a2e09d9ee4
Alex <119462299+RaphaelsHub@users.noreply.github.com>
2025-02-21 15:02:17 -0800

    Added realisation to IBlogsRepository.cs

--- ECommerce/ECommerce.Core/Interfaces/Blog/IBlogsRepository.cs
+++ ECommerce/ECommerce.Core/Interfaces/Blog/IBlogsRepository.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using ECommerce.Core.Entities.Blog;
 
 namespace ECommerce.Core.Interfaces.Blog
 {
-    public interface IBlogsRepository<T> : IGenericRepository<BlogEf>
+    public interface IBlogsRepository : IGenericRepository<BlogEf>
     {
-        
+        Task<IEnumerable<BlogEf>> GetPaginatedBlogsByNameAsync(string blogName, int page, int pageSize);
+        Task<IEnumerable<BlogEf>> GetLatestBlogsAsync(int amountOfItems);
     }
 }
--- ECommerce/ECommerce.Dal/Repositories/Blog/BlogsRepository.cs
+++ ECommerce/ECommerce.Dal/Repositories/Blog/BlogsRepository.cs
@@ -1,46 +1,62 @@
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
 using System.Threading.Tasks;
 using ECommerce.Core.Entities.Blog;
 using ECommerce.Core.Interfaces.Blog;
+using ExpressMapper;
 
 namespace ECommerce.Dal.Repositories.Blog
 {
-    public class BlogsRepository : IBlogsRepository<BlogEf>
+    public class BlogsRepository : IBlogsRepository
     {
         private readonly StoreContext _context;
         
-        public BlogsRepository(StoreContext context)
-        {
-            _context = context;
-        }
+        public BlogsRepository(StoreContext context) => _context = context;
         
-        public async Task<IEnumerable<BlogEf>> GetAllAsync() =>
-            await _context.Blogs.ToListAsync();
-
-        public async Task<BlogEf> GetByIdAsync(int id) =>
-            await _context.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
-
-        public async Task CreateAsync(BlogEf entity)
+        public async Task<BlogEf> GetByIdAsync(int id) => 
+            await _context.Blogs.FindAsync(id);
+        
+        public async Task<BlogEf> CreateAsync(BlogEf entity)
         {
             _context.Blogs.Add(entity);
             await _context.SaveChangesAsync();
+            return entity;
         }
 
-        public async Task UpdateAsync(BlogEf entity)
+        public async Task<BlogEf> UpdateAsync(BlogEf entity)
         {
-            _context.Blogs.Attach(entity);
+            var existingBlog = await _context.Blogs.FindAsync(entity.BlogId);
+            if (existingBlog == null) return null;
+            
+            Mapper.Map(entity, existingBlog);
+
             await _context.SaveChangesAsync();
+            return existingBlog;
         }
 
         public async Task DeleteByIdAsync(int id)
         {
-            var blog = await _context.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
-            if (blog != null)
+            var blogEf = await _context.Blogs.SingleOrDefaultAsync(b => b.BlogId == id);
+            if (blogEf != null)
             {
-                _context.Blogs.Remove(blog);
+                _context.Blogs.Remove(blogEf);
                 await _context.SaveChangesAsync();
             }
+        }
+
+
+        public async Task<IEnumerable<BlogEf>> GetPaginatedBlogsByNameAsync(string blogName, int page, int pageSize)
+        {
+            var request =  _context.Blogs.AsQueryable();
+            if (!string.IsNullOrEmpty(blogName)) request = request.Where(x => x.Title.Contains(blogName));
+            request = request.Skip((page - 1) * pageSize).Take(pageSize);
+            return await request.ToListAsync();
+        }
+
+        public async Task<IEnumerable<BlogEf>> GetLatestBlogsAsync(int amountOfItems)
+        {
+            return await _context.Blogs.OrderByDescending(x=>x.DateTimePublished).Take(amountOfItems).ToListAsync();
         }
     }
 }
