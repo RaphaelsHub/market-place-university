d49fcb764bc759b2a9eeded0be4f99ea5d0d7ee3
Alex <119462299+RaphaelsHub@users.noreply.github.com>
2025-02-22 22:32:26 -0800

    Base controller minimized.

--- ECommerce/ECommerce.Api/Controllers/BaseController.cs
+++ ECommerce/ECommerce.Api/Controllers/BaseController.cs
@@ -1,17 +1,11 @@
 using System;
 using System.Web.Mvc;
-using ECommerce.Helper;
 
 namespace ECommerce.Controllers
 {
     public class BaseController : Controller
     {
-        protected string GetLastUrl() => Request.UrlReferrer?.ToString() ?? "/";
-        
-        protected ActionResult RedirectToLastUrl() => Redirect(GetLastUrl());
-        
-        protected  ActionResult RedirectToErrorPage(int errorCode) 
-            =>  RedirectToAction("Error", "Home", new 
-                { errorCode, errorMessage = ErrorHelper.GetErrorMessage(errorCode) });
+        protected string GetLastUrl() => Request.UrlReferrer?.ToString() ?? "/Home";
+        protected ActionResult RedirectToLastPage() => Redirect(GetLastUrl());
     }
 }
