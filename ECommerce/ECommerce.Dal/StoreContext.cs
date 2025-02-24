1996b850590580104ef332480751796a1a2abeed
Alex <119462299+RaphaelsHub@users.noreply.github.com>
2025-02-23 20:16:14 -0800

    Deleted some of tables.

--- ECommerce/ECommerce.Dal/StoreContext.cs
+++ ECommerce/ECommerce.Dal/StoreContext.cs
@@ -2,7 +2,6 @@ using System;
 using System.Configuration;
 using System.Data.Entity;
 using System.Data.SqlClient;
-using System.Data.Entity.SqlServer;
 using ECommerce.Core.Entities.Blog;
 using ECommerce.Core.Entities.Product;
 using ECommerce.Core.Entities.User;
@@ -14,17 +13,12 @@ namespace ECommerce.Dal
     {
         public DbSet<UserEf> Users { get; set; }
         public DbSet<OrderEf> Orders { get; set; }
-        public DbSet<OrderItemEf> OrderItems { get; set; }
-        public DbSet<AddressEf> Addresses { get; set; }
-        public DbSet<CartItemEf> CartItems { get; set; }
+        public DbSet<ContactUsEf> ContactUs { get; set; }
+        public DbSet<BlogEf> Blogs { get; set; }
         public DbSet<ProductEf> Products { get; set; }
-        public DbSet<RateItemEf> RateItems { get; set; }
         public DbSet<CategoryEf> Categories { get; set; }
         public DbSet<FilterEf> Filters { get; set; }
         public DbSet<FilterValueEf> FilterValues { get; set; }
-        public DbSet<BlogEf> Blogs { get; set; }
-        public DbSet<MessageEf> Messages { get; set; }
-        public DbSet<ContactUsEf> ContactUs { get; set; }
         
         public StoreContext() : 
             base("DefaultConnection") => Connect(); 
@@ -79,7 +73,7 @@ namespace ECommerce.Dal
                 }
                 catch (Exception e)
                 {
-                    throw new Exception("Error: " + e.Message);
+                    return false;
                 }
             }
         }
@@ -95,7 +89,6 @@ namespace ECommerce.Dal
             modelBuilder.Configurations.Add(new CategoryEfConfiguration());
             modelBuilder.Configurations.Add(new FilterEfConfiguration());
             modelBuilder.Configurations.Add(new FilterValueEfConfiguration());
-            modelBuilder.Configurations.Add(new RateItemEfConfiguration());
             modelBuilder.Configurations.Add(new BlogEfConfiguration());
             modelBuilder.Configurations.Add(new MessageEfConfiguration());
             modelBuilder.Configurations.Add(new ContactUsEfConfiguration());
