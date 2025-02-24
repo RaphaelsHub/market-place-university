ba080f1c8dea1f1d215cd73aac6c6503ae66aa2b
Alex <119462299+RaphaelsHub@users.noreply.github.com>
2025-02-23 20:16:46 -0800

    Renamed const key to "Message".

--- ECommerce/ECommerce.Core/Constants/TempDataKeys.cs
+++ ECommerce/ECommerce.Core/Constants/TempDataKeys.cs
@@ -3,6 +3,6 @@ namespace ECommerce.Core.Constants
     public static class TempDataKeys
     {
         public const string Error = "Error";
-        public const string MessageResponse = "MessageResponse";
+        public const string ConfirmationMessageResponse = "MessageResponse";
     }
 }
