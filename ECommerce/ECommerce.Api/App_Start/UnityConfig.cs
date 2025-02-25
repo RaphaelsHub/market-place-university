1d8f9b0ae3003281f84e49590950e52e73143db7
Alex <119462299+RaphaelsHub@users.noreply.github.com>
2025-02-24 23:47:33 -0800

    Moved all registrations to UnityConfig.cs

--- ECommerce/ECommerce.Api/App_Start/UnityConfig.cs
+++ ECommerce/ECommerce.Api/App_Start/UnityConfig.cs
@@ -1,46 +1,91 @@
 using System;
+using ECommerce.App.Infrastructure.Abstractions;
+using ECommerce.App.Infrastructure.Services;
+using ECommerce.App.Interfaces.Blog;
+using ECommerce.App.Interfaces.JWT;
+using ECommerce.App.Interfaces.Product;
+using ECommerce.App.Interfaces.User;
+using ECommerce.App.Services.Blog;
+using ECommerce.App.Services.JWT;
+using ECommerce.App.Services.Product;
+using ECommerce.App.Services.User;
+using ECommerce.Core.Interfaces.Blog;
+using ECommerce.Core.Interfaces.Product;
+using ECommerce.Core.Interfaces.User;
+using ECommerce.Dal;
+using ECommerce.Dal.Repositories.Blog;
+using ECommerce.Dal.Repositories.Product;
+using ECommerce.Dal.Repositories.User;
 using Unity;
 
 namespace ECommerce 
 {
-    /// <summary>
-    /// Specifies the Unity configuration for the main container.
-    /// </summary>
     public static class UnityConfig
     {
         #region Unity Container
-        private static Lazy<IUnityContainer> container =
+        private static Lazy<IUnityContainer> _container =
           new Lazy<IUnityContainer>(() =>
           {
               var container = new UnityContainer();
               RegisterTypes(container);
               return container;
           });
-
-        /// <summary>
-        /// Configured Unity Container.
-        /// </summary>
-        public static IUnityContainer Container => container.Value;
+        
+        public static IUnityContainer Container => _container.Value;
         #endregion
-
-        /// <summary>
-        /// Registers the type mappings with the Unity container.
-        /// </summary>
-        /// <param name="container">The unity container to configure.</param>
-        /// <remarks>
-        /// There is no need to register concrete types such as controllers or
-        /// API controllers (unless you want to change the defaults), as Unity
-        /// allows resolving a concrete type even if it was not previously
-        /// registered.
-        /// </remarks>
+        
+        
         public static void RegisterTypes(IUnityContainer container)
         {
-            // NOTE: To load from web.config uncomment the line below.
-            // Make sure to add a Unity.Configuration to the using statements.
-            // container.LoadConfiguration();
+            /*
+             * NOTE: To load from web.config uncomment the line below.
+             * Make sure to add a Unity.Configuration to the using statements.
+             *container.LoadConfiguration();
+             */
+            
+            RegisterDbContext(container);
+            RegisterRepositories(container);
+            RegisterServices(container);
+        }
 
-            // TODO: Register your type's mappings here.
-            // container.RegisterType<IProductRepository, ProductRepository>();
+        private static void RegisterServices(IUnityContainer container)
+        {
+            //bll
+            container.RegisterType<IUserService, UserService>();
+            container.RegisterType<IOrderService, OrderService>();
+            container.RegisterType<ICartService, CartService>();
+            container.RegisterType<IContactUsService, ContactUsService>();
+            container.RegisterType<IAuthenticationService, AuthenticationService>();
+            container.RegisterType<IJwtService, JwtService>();
+            container.RegisterType<ICategoryService, CategoryService>();
+            container.RegisterType<IProductService, ProductService>();
+            container.RegisterType<IBlogService, BlogService>();
+            
+            //infrastructure
+            container.RegisterType<ICookiesService, CookiesService>();
+            container.RegisterType<ICacheService, CacheService>();
+            container.RegisterType<ISessionService, SessionService>();
+        }
+
+        private static void RegisterRepositories(IUnityContainer container)
+        {
+            container.RegisterType<IUsersRepository, UsersRepository>();
+            container.RegisterType<IOrdersRepository, OrdersRepository>();
+            container.RegisterType<ICartRepository, CartRepository>();
+            container.RegisterType<IContactUsRepository, ContactUsRepository>();
+            
+            container.RegisterType<IProductsRepository, ProductsRepository>();
+            container.RegisterType<ICategoriesRepository, CategoriesRepository>();
+            container.RegisterType<IFiltersRepository, FiltersRepository>();
+            container.RegisterType<IFilterValuesRepository, FilterValuesRepository>();
+            
+            container.RegisterType<IBlogsRepository, BlogsRepository>();
+            container.RegisterType<IMessagesRepository, MessagesRepository>();
+        }
+
+        private static void RegisterDbContext(IUnityContainer container)
+        {
+            container.RegisterType<StoreContext>();
         }
     }
 }
--- ECommerce/ECommerce.Api/Global.asax.cs
+++ ECommerce/ECommerce.Api/Global.asax.cs
@@ -1,38 +1,8 @@
-﻿using System;
+﻿using Unity;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
-using ECommerce.App.Infrastructure.Abstractions;
-using ECommerce.App.Infrastructure.Services;
-using ECommerce.App.Interfaces.Admin;
-using ECommerce.App.Interfaces.Auth;
-using ECommerce.App.Interfaces.Blog;
-using ECommerce.App.Interfaces.JWT;
-using ECommerce.App.Interfaces.Product;
-using ECommerce.App.Interfaces.User;
-using ECommerce.App.Services.Admin;
-using ECommerce.App.Services.Auth;
-using ECommerce.App.Services.Blog;
-using ECommerce.App.Services.JWT;
-using ECommerce.App.Services.Product;
-using ECommerce.App.Services.User;
-using ECommerce.Controllers;
-using ECommerce.Core.Entities.Blog;
-using ECommerce.Core.Entities.Product;
-using ECommerce.Core.Entities.User;
-using ECommerce.Core.Interfaces.Admin;
-using ECommerce.Core.Interfaces.Blog;
-using ECommerce.Core.Interfaces.Product;
-using ECommerce.Core.Interfaces.User;
-using ECommerce.Dal;
-using ECommerce.Dal.Repositories.Admin;
-using ECommerce.Dal.Repositories.Blog;
-using ECommerce.Dal.Repositories.Product;
-using ECommerce.Dal.Repositories.User;
-using ECommerce.Helper;
-using Unity;
-using Unity.AspNet.Mvc;
+using System.Web.Optimization;
 
 namespace ECommerce
 {
@@ -44,69 +14,10 @@ namespace ECommerce
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.ConfigureRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            
-            var container = new UnityContainer();
-            RegisterTypes(container);
-            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
-            
-            // ConfigureJwtAuth();      
+            MappingConfig.RegisterMappings();
+            UnityConfig.RegisterTypes(new UnityContainer());
         }
-
-        // private void ConfigureJwtAuth()
-        // {
-        //     throw new NotImplementedException();
-        // }
-
-        private void RegisterTypes(UnityContainer container)
-        {
-            // Регистрация зависимостей среди сервисов
-            // auth
-            container.RegisterType<IAccountManagementService, AccountManagementService>();
-            container.RegisterType<IAuthenticationService, AuthenticationService>();
-            container.RegisterType<IAuthorizationService, AuthorizationService>();
-            // admin
-            container.RegisterType<IAdminService, AdminService>();
-            // user
-            container.RegisterType<IUserService, UserService>();
-            container.RegisterType<IOrderService, OrderService>();
-            container.RegisterType<ICartService, CartService>();
-            // product
-            container.RegisterType<IProductService, ProductService>();
-            container.RegisterType<ICategoryService, CategoryService>();
-            // blog
-            container.RegisterType<IBlogService, BlogService>();
-
-            container.RegisterType<ICookiesService, CookiesService>();
-            container.RegisterType<IJwtService, JwtService>();
-            container.RegisterType<IContactUsService, ContactUsService>();
-            
-            // Регистрация зависимостей среди репозиториев
-            // User
-            container.RegisterType<IUsersRepository, UsersRepository>();
-            container.RegisterType<IOrdersRepository<OrderEf>, OrdersRepository>();
-            container.RegisterType<IOrderItemsRepository<OrderItemEf>,OrderItemsRepository>();
-            container.RegisterType<ICartItemsRepository<CartItemEf>,CartItemsRepository>();
-            container.RegisterType<IAddressesRepository<AddressEf>,AddressesRepository>();
-            container.RegisterType<IContactUsRepository, ContactUsRepository>();
-            
-            // Product
-            container.RegisterType<IProductsRepository<ProductEf>, ProductsRepository>();
-            container.RegisterType<ICategoriesRepository<CategoryEf>, CategoriesRepository>();
-            container.RegisterType<IFiltersRepository<FilterEf>,FiltersRepository>();
-            container.RegisterType<IFilterValuesRepository<FilterValueEf>,FilterValuesRepository>();
-            container.RegisterType<IRateItemsRepository<RateItemEf>,RateItemsRepository>();
-            
-            // Blog
-            container.RegisterType<IBlogsRepository<BlogEf>, BlogsRepository>();
-            container.RegisterType<IMessagesRepository<MessageEf>,MessagesRepository>();
-            
-            // WebSite
-            container.RegisterType<IWebSiteControl,WebSiteControl>();
-            
-            // Регистрация контекста базы данных
-            container.RegisterType<StoreContext>();
-        }
-
+        
         // protected void Application_Error(object sender, EventArgs e)
         // {
         //     var exception = Server.GetLastError();
