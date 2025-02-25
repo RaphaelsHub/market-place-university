b2a94828859f00923594ccfaa3e6d3ada579f34b
Alex <119462299+RaphaelsHub@users.noreply.github.com>
2025-02-24 23:48:32 -0800

    User refactoring.

--- ECommerce/ECommerce.App/Interfaces/User/IUserService.cs
+++ ECommerce/ECommerce.App/Interfaces/User/IUserService.cs
@@ -1,21 +1,18 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using ECommerce.Core.Enums.User;
+using ECommerce.Core.Models.DTOs.GenericResponses;
 using ECommerce.Core.Models.DTOs.User;
-using ECommerce.Core.Models.ViewModels;
 
 namespace ECommerce.App.Interfaces.User
 {
-    /// <summary>
-    ///  User service, which allows to create, read, update, delete user, find user by email, find user by username
-    /// </summary>
     public interface IUserService
     {
-        Task<bool> CreateUserAsync(UserDto user);
-        Task<ResponseViewModel<UserDto>> GetUserAsync(int id);
-        Task<ResponseViewModel<UserDto>> FindUserByEmailAsync(string email);
-        Task<ResponseViewModel<UserDto>> FindUserByUserNameAsync(string userName, int currentPage = 1, int amountOfUsers = 16);
-        Task<ResponseViewModel<IEnumerable<UserDto>>> GetUsersAsync(int currentPage = 1, int amountOfUsers = 16);
-        Task<bool> UpdateUserAsync(UserDto user);
-        Task<bool> DeleteUserAsync(int id);
+        Task<BaseResponse<UserDto>> CreateUserAsync(SignUpDto signUpDto);
+        Task<BaseResponse<UserDto>> UpdateUserAsync(UserDto userDto);
+        Task<BaseResponse<bool>> DeleteUserByIdAsync(int id);
+        Task<BaseResponse<UserDto>> GetUserByIdAsync(int id);
+        Task<BaseResponse<UserDto>> GetUserByEmailAsync(string email);
+        Task<BaseResponse<List<UserDto>>> GetUsersAsync(string email, UserType userType , int currentPage , int amountOfUsers); 
     }
 }
--- ECommerce/ECommerce.Core/Interfaces/User/IUsersRepository.cs
+++ ECommerce/ECommerce.Core/Interfaces/User/IUsersRepository.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using ECommerce.Core.Entities.User;
+using ECommerce.Core.Enums.User;
 
 namespace ECommerce.Core.Interfaces.User
 {
     public interface IUsersRepository : IGenericRepository<UserEf>
     {
         Task<UserEf> GetByEmailAsync(string email);
+        Task<IEnumerable<UserEf>> GetPaginatedUsersByEmailAndTypeAsync(string email, UserType userType, int page, int pageSize);
     }
 }
